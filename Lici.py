5ec235a7d02a2c8a61223158213c41582598dbfe
Pdiceneged <144238368+Pdiceneged@users.noreply.github.com>
2024-02-01 12:35:20 -0300

    Lici.py

--- Lici.py
+++ Lici.py
@@ -1,6 +1,41 @@
 import streamlit as st
 import requests
 import json
+import base64
+
+st.set_page_config(
+    page_title="Licitalert",
+    page_icon="🏛️"
+)
+@st.cache_data()
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64("fundocontrat.png")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+    background-image: url("data:fundoesg4k/png;base64,{img}");
+    background-size: 100%;
+    background-position: top left;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}}
+
+[data-testid="stHeader"] {{
+    background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+    right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 def coletar_licitacoes(url, palavras_chave, pagina, token):
     palavras_chave_str = ",".join(palavras_chave)
